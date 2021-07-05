caf8659deb54862aad5868146e78d68ac5436cfa
Sibel Eren <edaeren575@gmail.com>
2021-07-05 16:49:11 +0100

    completed #7 of the challenge

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,17 @@ class Oystercard
 
   attr_reader :balance
 
+  TOP_UP_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    fail "Your top up limit is £#{TOP_UP_LIMIT}." if @balance + money > TOP_UP_LIMIT
+    
     @balance += money
+
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,9 @@ describe Oystercard do
     oystercard.top_up(10)
     expect(oystercard.balance).to eq(10)
   end
+
+  it 'shouldnt exceed maximum top up' do
+    oystercard.top_up(100)
+    expect {oystercard.top_up 20 }.to raise_error "Your top up limit is £#{TOP_UP_LIMIT}."
+  end
 end
