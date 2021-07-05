5201ac864c708d6635c6063dc1238d46d30af0a4
Pav Rao <pav0107@gmail.com>
2021-07-05 16:22:46 +0100

    step 4 and 5, added balance and top-up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,17 @@
-describe Oystercard do
-  
+require 'oystercard'
 
+describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+  it 'has a balance' do
+    expect(oystercard).to respond_to(:balance)
+  end
+
+  it 'has a default balance of 0' do
+    expect(oystercard.balance).to eq(0)
+  end
+
+  it 'increases the balance by the top-up value' do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq(10)
+  end
 end
